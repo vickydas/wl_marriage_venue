bb217e8052e260089b69840ad617d05ee45df241
vickydas <vickydas435@gmail.com>
2025-11-21 06:15:18 +0530

    added some static media_url media to settings and urls conf

--- core/settings.py
+++ core/settings.py
@@ -175,3 +175,5 @@ DJOSER = {
 
 
 AUTH_USER_MODEL = 'users.User'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView  
 
@@ -36,4 +38,4 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),       
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
